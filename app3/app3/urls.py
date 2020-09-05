1a26bd2bdc416f019f2db46a3a7d17689f8fddab
schroederjacob <60017931+schroederjacob@users.noreply.github.com>
2020-09-04 19:09:52 -0600

    Update urls.py

--- app3/app3/urls.py
+++ app3/app3/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import about, home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home.as_view()),
+    path('about', about.as_view()),
 ]
