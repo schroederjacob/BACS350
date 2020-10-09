661ac113d896fa1453e8f638eeb58a3c7ea03473
schroederjacob <60017931+schroederjacob@users.noreply.github.com>
2020-10-09 14:44:12 -0600

    deleted files

--- SuperHeroProject/SuperHeroProject/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from pages.views import HomeView, BasePage, HeroView
-
-urlpatterns = [
-    path('', HeroView.as_view()),
-    path('<str:identity>', = HeroView.as_view())
-]
