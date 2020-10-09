593eb63479677a14f9c320879b95b732a42bbbee
schroederjacob <60017931+schroederjacob@users.noreply.github.com>
2020-10-09 10:05:11 -0600

    Update views.py

--- Project2/page/views.py
+++ Project2/page/views.py
@@ -3,5 +3,8 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = 'home.html'
 
-class AboutView(TemplateView):
-    template_name = 'about.html'
+class SpiderManView(TemplateView):
+    template_name = 'spiderman.html'
+
+class CapAmericaView(TemplateView):
+    template_name = 'captainamerica.html'
