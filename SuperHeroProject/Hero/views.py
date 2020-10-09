21f2744c00904c9342419d66b62f6644bb4e4163
schroederjacob <60017931+schroederjacob@users.noreply.github.com>
2020-10-09 14:51:03 -0600

    Update views.py

--- SuperHeroProject/Hero/views.py
+++ SuperHeroProject/Hero/views.py
@@ -1,21 +1,11 @@
 from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
+from .models import Superhero
 
 class HeroView(TemplateView):
     template_name = "Hero.html"
 
     def get_context_data(self, **kwargs):
         heroes = Superhero.objects.all()
+#        heroes = ['SpiderMan', 'CaptainAmerica', 'IronMan', 'Daredevil', 'Wolverine']
         return {'heroes': heroes}
- 
-class AddHeroView(CreateView):
-    template_name = "AddHero.html"
-    model = Superhero
-    
-
-class BasePage(TemplateView):
-    template_name = "PageTheme.html"
-    
-    
-class AboutPage(TemplateView):
-    template_name = "About.html"
