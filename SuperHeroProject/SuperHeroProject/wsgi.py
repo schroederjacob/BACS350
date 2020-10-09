119f45e45fe6fc2dd3e3e9ad47f25eef9f5c260c
schroederjacob <60017931+schroederjacob@users.noreply.github.com>
2020-10-09 14:02:59 -0600

    Update wsgi.py

--- SuperHeroProject/SuperHeroProject/wsgi.py
+++ SuperHeroProject/SuperHeroProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Project2 project.
+WSGI config for SuperHeroProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SuperHeroProject.settings')
 
 application = get_wsgi_application()
