65d4bbd0b6e760c6034a88aefd7a1ad3b312841a
schroederjacob <60017931+schroederjacob@users.noreply.github.com>
2020-10-09 14:52:33 -0600

    Update urls.py

--- SuperHeroProject/Hero/urls.py
+++ SuperHeroProject/Hero/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from pages.views import HeroView
+from Hero.views import HeroView
 
 urlpatterns = [
     path('', HeroView.as_view()),
