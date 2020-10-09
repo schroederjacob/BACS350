d187b72ef0649e633f1ecc1443ca6dbcf4499af8
schroederjacob <60017931+schroederjacob@users.noreply.github.com>
2020-10-09 13:19:02 -0600

    Update settings.py

--- Project2/Project2/settings.py
+++ Project2/Project2/settings.py
@@ -119,4 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT =  '/home/jacobschroeder/BACS350/static'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
