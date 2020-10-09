def091cd38011aa4276b93848e93722a42719389
schroederjacob <60017931+schroederjacob@users.noreply.github.com>
2020-10-09 14:47:35 -0600

    Update tests.py

--- SuperHeroProject/Hero/tests.py
+++ SuperHeroProject/Hero/tests.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 
 # Add tests here.
-class Test(SimpleTestCase):
+class Tests(SimpleTestCase):
   def test_index_view(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         
-        response = self.client.get('spiderman')
+        response = self.client.get('SpiderMan')
         self.assertEqual(response.status_code, 200)
