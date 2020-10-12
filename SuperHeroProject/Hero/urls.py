18bb909b9f38024d9269142a797e88cf12254eef
schroederjacob <60017931+schroederjacob@users.noreply.github.com>
2020-10-12 13:54:20 -0600

    Update urls.py

--- SuperHeroProject/Hero/urls.py
+++ SuperHeroProject/Hero/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from Hero.views import HeroView, IndexView
+from Hero.views import HeroView, IndexView, BaseView
 
 urlpatterns = [
-    path('', HeroView.as_view()),
+    path('', IndexView.as_view()),
     path('index', IndexView.as_view()),
     path('<str:identity>', HeroView.as_view())
+    path('base', BaseView.as_view())
 ]
