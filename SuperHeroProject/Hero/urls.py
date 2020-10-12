7f2651c154298fedf9af989b0458fe9cdd65a406
schroederjacob <60017931+schroederjacob@users.noreply.github.com>
2020-10-12 12:00:23 -0600

    Update urls.py

--- SuperHeroProject/Hero/urls.py
+++ SuperHeroProject/Hero/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from Hero.views import HeroView, IndexView
 
 urlpatterns = [
-    path('', IndexView.as_view()),
-    path('<str:name>', HeroView.as_view())
+    path('', HeroView.as_view()),
+    path('index', IndexView.as_view()),
+    path('<str:identity>', HeroView.as_view())
 ]
