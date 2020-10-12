1fbdcc99943873b5732978149b399a53bd9e79e3
schroederjacob <60017931+schroederjacob@users.noreply.github.com>
2020-10-12 07:41:29 -0600

    Update views.py

--- SuperHeroProject/Hero/views.py
+++ SuperHeroProject/Hero/views.py
@@ -6,9 +6,9 @@ class HeroView(TemplateView):
     template_name = "Hero.html"
 
     def get_context_data(self, **kwargs):
-        heroes = Superhero.objects.all()
+        hero = Superhero.objects.all()
 #        heroes = ['SpiderMan', 'CaptainAmerica', 'IronMan', 'Daredevil', 'Wolverine']
-        return {'heroes': heroes}
+        return {'hero': hero}
 
 class IndexView(TemplateView):
     template_name = "index.html"
